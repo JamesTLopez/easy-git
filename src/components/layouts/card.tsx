b4cc1fd2ad7a925cde1fabdfe4a35932e37fff11
James <jtlopez919@gmail.com>
2020-08-28 15:04:31 -0400

    card
    -displays users repos on cards

--- src/components/layouts/card.tsx
+++ src/components/layouts/card.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 
 interface Props{
+    id:any;
     name:string;
+    owner_url:string;
+    html_url:string;
+    owner_name:string;
+
 }
 
-const card: React.FC<Props> = ({name}) => {
+const card: React.FC<Props> = ({id,name,owner_url,html_url,owner_name}) => {
     return (
-        <div className="card">
-            <p>{name}</p>
+        <div key={id} className="card">
+            <div className="card-container">
+                <div className="image-area">
+                    <img src={owner_url} alt="Owner Url"></img>
+                </div>
+                <div className="info-area">
+                    <label>{name}</label>
+                    <label>{owner_name}</label>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/pages/profile.tsx
+++ src/components/pages/profile.tsx
@@ -1,17 +1,18 @@
-import React,{useContext} from 'react'
+import React, { useContext } from 'react'
 
 import UserProvider from "../../context"
+import Card from "../layouts/card"
 
 
 const Profile: React.FC = () => {
-    const {state} = useContext(UserProvider);
+    const { state } = useContext(UserProvider);
     const UserData = state.userInfo;
+    const UserRepo = state.userRepos;
 
 
-    const updateState = () =>{
-        console.log(state);
-    
-     }
+    const updateState = () => {
+        console.log(UserRepo[0])
+    }
 
 
 
@@ -22,34 +23,62 @@ const Profile: React.FC = () => {
                     <div className="user-picture">
                         <img src={UserData.avatar_url} alt="Alt"></img>
                         <p>{UserData.login}</p>
-             
+
                         <button onClick={updateState}>State Changer Test</button>
                     </div>
                     <div className="user-info">
                         <div className="list">
-                            {/* <table>
-                                <tr>
-                                    <th>Login:</th>
-                                    <td>{UserData?.login}</td>
-                                </tr>
-                                <tr>
-                                    <th>Login:</th>
-                                    <td>{UserData?.login}</td>
-                                </tr>
-                            </table> */}
+                            <div className="row-1">
+                                <div className="item">
+                                    <label>Login: </label>
+                                    <p>{UserData.login}</p>
+                                </div>
+                                <div className="item">
+                                    <label>Name: </label>
+                                    <p>{UserData.name ? UserData.name : 'N/A'}</p>
+                                </div>
+                                <div className="item">
+                                    <label>Company: </label>
+                                    <p>{UserData.company ? UserData.company : 'N/A'}</p>
+                                </div>
+                                <div className="item">
+                                    <label>Location: </label>
+                                    <p>{UserData.location ? UserData.location : 'N/A'}</p>
+                                </div>
+                            </div>
+                            <div className="row-2">
+                                <div className="item">
+                                    <label>Hirable: </label>
+                                    <p>{UserData.hirable ? UserData.hirable : 'N/A'}</p>
+                                </div>
+                                <div className="item">
+                                    <label>Email: </label>
+                                    <p>{UserData.email ? UserData.email : 'N/A'}</p>
+                                </div>
+                                <div className="item">
+                                    <label>Followers: </label>
+                                    <p>{UserData.followers ? UserData.followers : 'N/A'}</p>
+                                </div>
+                                <div className="item">
+                                    <label>Following: </label>
+                                    <p>{UserData.following ? UserData.following : 'N/A'}</p>
+                                </div>
+
+                            </div>
                         </div>
-
                     </div>
-
                 </div>
                 <div className="profile-repos">
                     <div className="repository-list">
-                        
+                        {(UserRepo.length !== 0) ? (UserRepo.map((repo:any) => (
+                            // <h3 key={repo.id}>{repo.name}</h3>
+                            <Card key={repo.id} id={repo.id} name={repo.name} owner_url={repo.owner.avatar_url} html_url={repo.html_url} owner_name={repo.owner.login}/>
+                        ))) : <h2>No repository to be displayed</h2>}
 
-                        
+
 
                     </div>
-                </div>               
+                </div>
             </div>
         </div>
     )
