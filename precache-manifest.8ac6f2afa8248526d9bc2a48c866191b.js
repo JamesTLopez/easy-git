4cd5b0d8241fd03c8f981b49f4d8c5a61d257db2
James <jtlopez919@gmail.com>
2020-09-01 13:37:56 -0400

    Updates

---
+++ precache-manifest.8ac6f2afa8248526d9bc2a48c866191b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33eaae2025b7c54b31a42665ad022282",
+    "url": "/easy-git/index.html"
+  },
+  {
+    "revision": "9f034ec28c2deacefc3e",
+    "url": "/easy-git/static/css/main.b328cd5d.chunk.css"
+  },
+  {
+    "revision": "6b5910f241d2e2bd8a77",
+    "url": "/easy-git/static/js/2.5bdfff60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/easy-git/static/js/2.5bdfff60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f034ec28c2deacefc3e",
+    "url": "/easy-git/static/js/main.8816e853.chunk.js"
+  },
+  {
+    "revision": "41698606e9f1d709aa9f",
+    "url": "/easy-git/static/js/runtime-main.6be1eb73.js"
+  }
+]);
